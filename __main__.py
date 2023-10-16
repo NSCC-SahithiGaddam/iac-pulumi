5bbddff8fb9a9a076970ce078f993da3d2b99c08
Sahithi Gaddam <113363655+sahithir27@users.noreply.github.com>
2023-10-16 18:45:11 -0400

    removed hardcoded az values (#4)

--- __main__.py
+++ __main__.py
@@ -1,6 +1,10 @@
 import pulumi
 import pulumi_aws as aws
 
+zones = []
+public_subnets = []
+private_subnets = []
+
 config = pulumi.Config()
 vpc_name = config.require('vpc_name')
 igw_name = config.require('myigw_name')
@@ -20,27 +24,32 @@ create_igw = aws.ec2.InternetGateway("gw",
         "Name": igw_name,
     })
 
-public_subnets = []
-private_subnets = []
-for config in public_subnets_config:
-    subnet = aws.ec2.Subnet(config["name"],
-        vpc_id=create_vpc.id,
-        cidr_block=config["cidr_block"],
-        availability_zone=config["az"],  # Set your desired availability zone
-        tags={"Name": config["name"]},
+availability_zones = aws.get_availability_zones(
+    state="available"
+)
+availability_zone_count = len(availability_zones.names)
+if availability_zone_count > 3:
+    zones = availability_zones.names[:3]
+else:
+    zones = availability_zones.names
+
+for i,zone in enumerate(zones):
+    subnet = aws.ec2.Subnet(public_subnets_config[i]["name"],
+    vpc_id=create_vpc.id,
+    cidr_block=public_subnets_config[i]["cidr_block"],
+    availability_zone = zone,
+    tags={"Name": public_subnets_config[i]["name"]},
     )
     public_subnets.append(subnet)
 
-for config in private_subnets_config:
-    subnet = aws.ec2.Subnet(config["name"],
-        vpc_id=create_vpc.id,
-        cidr_block=config["cidr_block"],
-        availability_zone=config["az"],  # Set your desired availability zone
-        tags={"Name": config["name"]},
+for i,zone in enumerate(zones):
+    subnet = aws.ec2.Subnet(private_subnets_config[i]["name"],
+    vpc_id=create_vpc.id,
+    cidr_block=private_subnets_config[i]["cidr_block"],
+    availability_zone = zone,
+    tags={"Name": private_subnets_config[i]["name"]},
     )
     private_subnets.append(subnet)
-
-
 
 public_route_table = aws.ec2.RouteTable("public-route-table",
     vpc_id=create_vpc.id,
@@ -74,4 +83,6 @@ for i, subnet in enumerate(public_subnets):
     pulumi.export(f'publicsubnet-{i + 1}', subnet.id)
 for i, subnet in enumerate(private_subnets):
     pulumi.export(f'privatesubnet-{i + 1}', subnet.id)
+pulumi.export('az', zones)
+pulumi.export("availability_zone_count", availability_zone_count)
 
